a2ab501b9794bf533ec536423f574a8075bc2221
Rama Krishna <siddarth21@hotmail.com>
2016-03-17 20:38:43 +0800

    added Chart.js, and chart to dashboard

--- client/views/dashboard.js
+++ client/views/dashboard.js
@@ -5,18 +5,52 @@ TODO
 */
 Goals = new Mongo.Collection("goals");
 
-if (Meteor.isClient) {
 
-  // This code only runs on the client
- Template.dashboard.helpers({
- 		username: function () {
- 			return Meteor.user() && Meteor.user().username;
-  	}, 
+Template.dashboard.helpers({
+	username: function () {
+		return Meteor.user() && Meteor.user().username;
+	}, 
     goals: function () {
     	var this_user = Meteor.user() ? Meteor.user().username : "test_user1";
     	return Goals.find({user: this_user});
     }
-  });
+});
+
+Template.goalchart.onCreated(function(){
+	this.goal = this.data._id;
+});
+
+Template.goalchart.onRendered( function() {
+	var value = this.goal;
+	console.log(value);
+  	drawChart(value);
+	
+});
+
+function drawChart(goalid){
+	var goal = +(Goals.findOne({_id: goalid}).progress);
+	console.log(goal);
+  	var data = [
+	    {
+	        value: goal,
+	        color:"#e50b4f",
+	        highlight: "#e50b4f",
+	        label: "Current Value"
+	    },
+	    {
+	        value: 100 - goal,
+	        color: "#ccc",
+	        highlight: "#ccc",
+	        label: "Remaining Value"
+	    }
+	]
+
+	var ctx = document.getElementById(goalid).getContext("2d");
+	var myNewChart = new Chart(ctx);
+
+	new Chart(ctx).Pie(data);
 }
+
+
 
 
