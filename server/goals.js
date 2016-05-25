7801a40d797cad7190e9a0e35ca6fa3cf9ab2eca
zikhan <zikanghan@gmail.com>
2016-05-25 17:12:50 +0800

    some recommendation functionalities

--- server/goals.js
+++ server/goals.js
@@ -1,5 +1,6 @@
 Goals = new Mongo.Collection("goals");
-
+Goal_catalog = new Mongo.Collection("goal_catalog")
+Keywords = new Mongo.Collection("keywords");
 if (Meteor.isServer) {
   //Goals.remove({});
   var exec = Npm.require('child_process').exec;
@@ -8,6 +9,59 @@ if (Meteor.isServer) {
 
   Meteor.methods({
     start_up: function() {
+      /*
+            var keywords = ["Education", "Lifestyle", "Nature"];
+            var result = [];
+            var goal_count = 5;
+            for(var i=0;i<keywords.length;i++){
+                var key = keywords[i];
+                var res = Goal_catalog.find({keywords: key});
+                result.push({});
+                console.log(res.keywords);
+                var count = goal_count? goal_count < res.length: res.length;
+                for (var j=0;j<count;j++){
+                    result[i]["goals"].push(res[j]["goal"]);
+                }
+            }
+            console.log(result);
+      */
+      g = [{"keyword": "Education", "goals": ["Finish masters degree", "Get into a creative writing class", "Kid's college fund", "Go for an art class"]}
+          ,{"keyword": "Lifestyle", "goals": ["Own a Range Rover", "Own a yacht", "Ride a camel in the desert", "Travel by helicopter", "Stay in Japan for a month"]}
+          ,{"keyword": "Life Plans", "goals":  ["Plan for retirement", "Japanese old people's home", 'Live in NYC for a year', "Purchase our first home!", "have a kid"]}
+          ,{"keyword": "Life Milestone", "goals":  ["Buy Engagement Ring", "Pass Driving Test", "Have a kid", "Start a Family", "Buy my first car!"]}
+          ,{"keyword": "Sports", "goals":  ["Learn to Rock Climb", "Go to a Manchester United game", "Take up yoga", "Learn how to ballroom dance", "Go on diving trips"]}
+          ,{"keyword": "Nature", "goals":  ["Be on a boat during the sunset", "Camp on the Beach", "See the redwoods", "See Cherry Blossoms in Japan"]}
+          ,{"keyword": "Travel", "goals": ["Visit the Christmas Markets in Germany", "Visit the Osaka Castle", "Visit Macau", "Visit England", "Hear Big Ben chime on the hour in London", "Visit Tanzania",  "See the paddy fields of Vietnam"]} 
+          ,{"keyword": "Skills", "goals": ["Learn to Ride a Motorcycle", "Horseback riding", "Learn to play the Ukulele", "Take up Yoga"]}
+          ,{"keyword": "Fan activities", "goals": ["Go to all the Harry Potter locations", "Meet Mike Tyson"]}
+          ];
+          Keywords.remove({});
+      for(var i=0;i<g.length;i++){
+        var list = [];
+        for (var j=0;j<3;j++){
+          list.push(g[i]["goals"][i]);
+        }
+        Keywords.insert({
+          keyword: g[i]["keyword"],
+          goals: list
+        });
+        console.log(g[i]["keyword"]);
+      }
+        /*
+      for(var i=0;i<g.length;i++){
+        for (var j=0;j< g[i]["goals"].length;j++){
+          Goal_catalog.insert({
+            keywords: [g[i]["keyword"]],
+            goal: g[i]["goals"][j],
+            clicks: 0,
+            A: 0,
+            B: 0,
+            C: 0
+          });
+          console.log(g[i]["goals"][j]);
+        }
+      }
+     
       console.log(Meteor.user().username);
       var this_user = Meteor.user().username;
       if (Goals.find({user: this_user}).count() === 0) {
@@ -41,7 +95,8 @@ if (Meteor.isServer) {
           user: this_user, 
           details: "I'm lucky",
         });
-      }
+      } */
+
     }, 
     call_python: function() {
       var fut = new Future();
@@ -62,8 +117,8 @@ if (Meteor.isServer) {
       //return allocation;
       return fut.wait();
     },
-
   });
+
 }
 
 
