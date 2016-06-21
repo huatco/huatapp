5e7afc47b1ecc6ef1da586c9551a7c9851aecfbf
Rama Krishna <siddarth21@hotmail.com>
2016-06-21 21:52:02 +0800

    Added Facebook login functionality

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '514034155465356',
+    secret: 'c36fa952dc736986e5134aa30994fc01'
+});
