d8b3d08e3e25f12bff740d197c09397a932878ad
zikhan <zikanghan@gmail.com>
2016-07-10 16:06:49 +0800

    tidied up goal related functions

--- client/views/goal.js
+++ client/views/goal.js
@@ -1,9 +1,7 @@
+
 var investment;
 var periods;
 var start_date;
-var target_date;
-var present;
-var r;
 
 Template.goal.helpers({
 	createDate: function(timestamp) {
@@ -18,14 +16,13 @@ Template.goal.helpers({
 
 	investment: function(goalid){
 		var goal = Goals.findOne({_id: goalid});
-		r = Meteor.user().profile.return_rate;
+		var r = Meteor.user().profile.return_rate;
 		start_date = moment(goal.time_stamp);
-		target_date = moment([goal.goal_year, goal.goal_month]);
-		present = moment();
+		var target_date = moment([goal.goal_year, goal.goal_month]);
+		var present = moment();
 		periods = target_date.diff(start_date, 'months');
 		investment = goal.monthly_amt;
 		return parseFloat(Math.round(investment * 100) / 100).toFixed(2);
-
 	},
 
 	format_money: function(amt) {
@@ -46,7 +43,7 @@ Template.goal.events({
 	"click .delete_goal": function(event, template) {
         var goalid = template.data._id;
         Goals.remove(goalid);
-        Meteor.call("call_python", function(error) {});
+        //Meteor.call("call_python", function(error) {});
         Router.go('/bucketlist')
     },
 });
@@ -57,60 +54,9 @@ Template.goalprogressChart.onRendered( function(){
 });
 
 
-function monthlyInvestments(a, b) {
-	var curr_period = present.diff(start_date, 'months');
-	var investments = [];
-	var data = [];
-	var curr = 0;
-	for(i=0; i<periods; i++){
-		var val = curr + investment*(Math.pow(1+r,i));
-		investments.push(val.toFixed(2));
-		curr = val;
-	}
-
-	var data_start = present.diff(start_date, 'months') - a;
-
-	for(i=0; i<b; i++){
-		data.push(investments[data_start]);
-		data_start+=1;
-	}
-
-	return data;
-};
-
-function monthLabels(a,b) {
-	var labels = [];
-	var m = moment();
-	var curr = m.month() - a;
-	for (var i = 0; i < b; i++) {
-		var mon = curr%12;
-		labels.push(m.month(mon).format('MMMM'));
-		curr+=1;
-	}
-	return labels;
-};
-
-function datalength(){
-	var curr_period = present.diff(start_date, 'months');
-	var a;
-	var b;
-	if(curr_period<5){
-		a = curr_period;
-	}else{
-		a = 5;
-	}
-
-	if(periods<12){
-		b = periods;
-	}else{
-		b= 12;
-	}
-
-	return [a,b]
-};
-
 function drawChart() {
-	var vals = datalength();
+	var curr_period = moment().diff(start_date, 'months');
+	var vals = [curr_period < 5 ? curr_period : 5, periods < 12 ? periods : 12]
     var data2 = monthlyInvestments(vals[0], vals[1]);
     var monthlabels = monthLabels(vals[0], vals[1]);
     console.log(vals);
@@ -146,4 +92,40 @@ function drawChart() {
     myBarChart.datasets[0].bars[vals[0]].fillColor = "rgba(229, 11, 79,0.65)"; 
     myBarChart.update();
 
+};
+
+
+function monthlyInvestments(a, b) {
+
+	var curr_period = moment().diff(start_date, 'months');
+	var investments = [];
+	var data = [];
+	var curr = 0;
+	for(i=0; i<periods; i++){
+		var val = curr + investment*(Math.pow(1+r,i));
+		investments.push(val.toFixed(2));
+		curr = val;
+	}
+
+	var data_start = moment().diff(start_date, 'months') - a;
+
+	for(i=0; i<b; i++){
+		data.push(investments[data_start]);
+		data_start+=1;
+	}
+
+	return data;
+};
+
+
+function monthLabels(a,b) {
+	var labels = [];
+	var m = moment();
+	var curr = m.month() - a;
+	for (var i = 0; i < b; i++) {
+		var mon = curr%12;
+		labels.push(m.month(mon).format('MMMM'));
+		curr+=1;
+	}
+	return labels;
 };
--- server/goals.js
+++ server/goals.js
@@ -1,4 +1,4 @@
-import {returnRate, assign_bucket, SAMPLE_GOALS, SAMPLE_KEYWORDS} from "../investment.js"
+import {returnRate, assign_bucket, SAMPLE_KEYWORDS} from "../investment.js"
 
 Goals = new Mongo.Collection("goals");
 Goal_catalog = new Mongo.Collection("goal_catalog");
@@ -21,154 +21,94 @@ Goals.schema = new SimpleSchema({
   keywords: {type: String, optional: true}
 });
 
-if (Meteor.isServer) {
-  /*
-  var exec = Npm.require('child_process').exec;
-  var Fiber = Npm.require('fibers');
-  var Future = Npm.require('fibers/future');
-  */
-  Meteor.methods({
-    recommendation: function(key){
-      var keys = key[0];
-      var dkeys = key[1];
-      Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.rec_keywords": keys}});
-      Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.dislike_keywords": dkeys}}); 
-    },
-    topup: function (increment) {
-      Meteor.users.update(Meteor.userId(), {
-        $set: {
-          "profile.increment": increment,
-        }
-      });
-      console.log("INCREMENTED");
-    }, 
-    keyword_clean_up: function () {
-      var keys = [];
-      for (var i = 0; i < SAMPLE_KEYWORDS.length; i++){
-        keys.push(SAMPLE_KEYWORDS[i].title);
-      }
-        
-      Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.rec_keywords": keys}});
-      Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.dislike_keywords": []}}); 
-    },
 
-    start_up: function () {
-      //populate goal catalog if it's not already populated
-      for(var i=0;i<SAMPLE_GOALS.length;i++)
-          for (var j=0;j<SAMPLE_GOALS[i]["goals"].length;j++)
-            if (Goal_catalog.find({goal: SAMPLE_GOALS[i]["goals"][j]}).count()==0)
-              Goal_catalog.insert(
-                {
-                  "keywords": [SAMPLE_GOALS[i]["keyword"]],
-                "goal": SAMPLE_GOALS[i]["goals"][j], "clicks": 0,
-                "A": 0, "B": 0, "C": 0
-                });
+Meteor.methods({
+
+  add_goal: function (title, month, year, priority, target_amt, category, desc, tags) {
+    var time_stamp = new Date();
+    console.log(time_stamp);
+    console.log(tags);
+    console.log(time_stamp.getYear());
+    var new_goal = {
+      title: title,
+      time_stamp: time_stamp,
+      created_year: time_stamp.getYear(),
+      created_month: time_stamp.getMonth(),
+      goal_month: parseInt(month),
+      goal_year: parseInt(year),
+      priority: parseInt(priority),
+      target_amount: parseFloat(target_amt),
+      current_amount: 0.0,
+      progress: 0,
+      category: category,
+      user: Meteor.user().username,
+      details: desc,
+      current_time: 0,
+      keywords: tags
+    };
+    var context = Goals.schema.namedContext("add_goal");
+    if (context.validate(new_goal)) {
+      Goals.insert(new_goal);
+      return true;
+    } else {
+      console.log(context.invalidKeys());
+      return false;
+    }
+  },
+
+
+  recommendation: function(key){
+    var keys = key[0];
+    var dkeys = key[1];
+    Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.rec_keywords": keys}});
+    Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.dislike_keywords": dkeys}}); 
+  },
+
+  
+  topup: function (increment) {
+    Meteor.users.update(Meteor.userId(), {
+      $set: {
+        "profile.increment": increment,
+      }
+    });
+    console.log("INCREMENTED");
+  }, 
+
+  
+  keyword_clean_up: function () {
+    var keys = [];
+    for (var i = 0; i < SAMPLE_KEYWORDS.length; i++){
+      keys.push(SAMPLE_KEYWORDS[i].title);
+    }
       
-      var monthly_requirement = 0
-      var total_requirement = 0
-      var goalTable = {}
-      //var bucket = assign_bucket()
-      var goals = Goals.find({user: Meteor.user().username})
-      var score = Meteor.user().profile.risk_score;
-      var rate = returnRate(score)
-     
-      goals.forEach(function(goal){
-        var amt = investmentAmt(goal.target_amount, goal.time_stamp, goal.goal_month, goal.goal_year, rate);
-        var p = targetPeriod(goal.goal_month, goal.goal_year);
+    Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.rec_keywords": keys}});
+    Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.dislike_keywords": []}}); 
+  },
 
-        (p in goalTable) ? goalTable[p] += goal.target_amount : goalTable[p] = goal.target_amount;
-
-        monthly_requirement += amt;
-        total_requirement += parseFloat(goal.target_amount);
-        Goals.update({_id: goal._id}, {$set: {monthly_amt: amt}});
-      });
-      Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.return_rate": rate,
-            "profile.monthly_require": monthly_requirement, "profile.total_require": total_requirement,
-            "profile.goal_table": goalTable}});
-    },
+}); 
 
 
-    add_goal: function (title, month, year, priority, target_amt, category, desc, tags) {
-      var time_stamp = new Date();
-      console.log(time_stamp);
-      console.log(tags);
-      console.log(time_stamp.getYear());
-      var new_goal = {
-        title: title,
-        time_stamp: time_stamp,
-        created_year: time_stamp.getYear(),
-        created_month: time_stamp.getMonth(),
-        goal_month: parseInt(month),
-        goal_year: parseInt(year),
-        priority: parseInt(priority),
-        target_amount: parseFloat(target_amt),
-        current_amount: 0.0,
-        progress: 0,
-        category: category,
-        user: Meteor.user().username,
-        details: desc,
-        current_time: 0,
-        keywords: tags
-      };
-      var context = Goals.schema.namedContext("add_goal");
-      if (context.validate(new_goal)) {
-        Goals.insert(new_goal);
-        return true;
-      } else {
-        console.log(context.invalidKeys());
-        return false;
-      }
-
-    },
-
-
-    /*
-    call_python: function() {
-      var fut = new Future();
-      var allocation;
-      var this_user = Meteor.user() ? Meteor.user().username : "test_user1";
-      var url = process.env.MONGO_URL;
-      console.log("Server running goal sorting optimization...")
-      console.log("Mongo url:", url);
-      console.log("Username:", this_user);
-      //console.log(__dirname);
-      var cmd = 'python ../../../../../goal_sorting.py ' + this_user + ' ' + url
-      //var cmd = "ls ../../../../..";
-      exec(cmd, function (error, stdout, stderr) {
-        console.log("Standard output from python script: ")
-        console.log(stdout);
-        console.log(stderr);
-      });
-      //return allocation;
-      return fut.wait();
-    },
-    */
-  }); 
-
-};
-
-function investmentAmt(amount, start, month, year, r) {
   /*
-  r: rate of return
+var exec = Npm.require('child_process').exec;
+var Fiber = Npm.require('fibers');
+var Future = Npm.require('fibers/future');
+  call_python: function() {
+    var fut = new Future();
+    var allocation;
+    var this_user = Meteor.user() ? Meteor.user().username : "test_user1";
+    var url = process.env.MONGO_URL;
+    console.log("Server running goal sorting optimization...")
+    console.log("Mongo url:", url);
+    console.log("Username:", this_user);
+    //console.log(__dirname);
+    var cmd = 'python ../../../../../goal_sorting.py ' + this_user + ' ' + url
+    //var cmd = "ls ../../../../..";
+    exec(cmd, function (error, stdout, stderr) {
+      console.log("Standard output from python script: ")
+      console.log(stdout);
+      console.log(stderr);
+    });
+    //return allocation;
+    return fut.wait();
+  },
   */
-  var start_date = moment(start);
-  var target_date = moment([year, month]);
-  var periods = target_date.diff(start_date, 'months');
-  var denom = 0;
-  for(i=0; i<periods; i++){
-    var val = Math.pow(1+r, i);
-    denom += val;
-  }
-  investment = amount/denom;
-  return investment;
-};
-
-function targetPeriod(month, year){
-  var present = moment();
-  var target = moment([year, month]);
-  var period = target.diff(present, 'months');
-  return period;
-}
-
-
-
